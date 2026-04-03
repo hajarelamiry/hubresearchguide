fd49c8dd15c0faf5b7569d92517b84b385dda692
Hajar Elamiry <elamiryhajar801@gmail.com>
2026-04-03 15:25:28 +0100

    changes

--- eslint.config.js
+++ eslint.config.js
@@ -18,9 +18,10 @@ export default tseslint.config(
       "react-refresh": reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "react-hooks/exhaustive-deps": "off",
     },
   },
 );
